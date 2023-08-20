512be3ef99240d36d433d1552aeee0ccf77239bf
Vikas Mulupuru <vikasmulupuru@localhost.localdomain>
2023-08-20 01:46:49 -0700

    fixed api call and connected login to stats page

--- listenify/src/components/Login/index.js
+++ listenify/src/components/Login/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './index.scss';
 import axios from 'axios';
+import SpotifyWebApi from 'spotify-web-api-js';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const clientId = 'e7c4f61b0cad4ef29625b7f21946d174';
     const redirectUri = "http://localhost:3000";
     const authEndpoint = "https://accounts.spotify.com/authorize"
     const responseType = "token"
+    const spotify = new SpotifyWebApi();
+    const [spotifyToken, setSpotifyToken] = useState("");
+    const navigate = useNavigate();
 
 
     // ** ADD THE ACTUAL SCOPES **
@@ -15,33 +21,40 @@ const Login = () => {
     const authURL = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}`
     // const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token`;
 
-    const token = window.location.hash
-        .substring(1)
-        .split('&')
-        .reduce((initial, item) => {
-            if (item) {
-            const parts = item.split('=');
-            initial[parts[0]] = decodeURIComponent(parts[1]);
-            }
-            return initial;
-        }, {});
+    const getTokenFromUrl = () => { 
+        return window.location.hash
+            .substring(1)
+            .split('&')
+            .reduce((initial, item) => {
+                let parts = item.split("=");
+                initial[parts[0]] = decodeURIComponent(parts[1])
 
-    const apiUrl = 'https://api.spotify.com/v1/';
+                return initial;
+            }, {});
+    }
+    const apiUrl = 'https://accounts.spotify.com/api/token';
 
-    // Make a sample API request
-    axios.get(`${apiUrl}`, {
-        headers: {
-            Authorization: `Bearer ${token.access_token}`,
-        },
-    })
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.error('Error fetching user data:', error);
-    });
+    useEffect(() => {
+        console.log("Derived: ", getTokenFromUrl())
+        const spotifytoken = getTokenFromUrl().access_token;
+        window.location.has = "";
+
+        console.log("SPOTIFY TOKEN: ", spotifytoken);
+
+        if (spotifytoken) {
+
+            setSpotifyToken(spotifytoken);
+
+            spotify.setAccessToken(spotifytoken);
+            spotify.getMe().then((user)=> {
+                console.log("THIS IS YOU: ", user)
+                navigate("/activity", { state: { person: user } });
+            });
+        }  
+    })  
+
     return (
-        <div class='container'>
+        <div className='container'>
             <h1 id='login-title'>Welcome to Listenify</h1>
             <a id='authorization-link' href={authURL}>Authorize with Spotify</a>
         </div>
--- listenify/src/components/Stats/index.js
+++ listenify/src/components/Stats/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './index.scss'
+import { useLocation } from 'react-router-dom';
 
 const Stats = () => {
+    const location = useLocation();
+    const person = location.state.person;
+    const profile_pic = person.images[0];
     return (
-        <h1>Stats page starts here.</h1>
+        <h1>Name: {person.display_name}</h1>
+        
     )
 }
 
