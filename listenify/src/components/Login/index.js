a41e039f0760932784de344d9889d8a6e98bec63
Leor Porat <lporat874@student.fuhsd.org>
2023-09-15 18:16:54 -0500

    Fix Auth Scope Issue

--- listenify/src/components/Login/index.js
+++ listenify/src/components/Login/index.js
@@ -1,29 +1,26 @@
 import React from 'react';
 import './index.scss';
 import axios from 'axios';
-import SpotifyWebApi from 'spotify-web-api-js';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
     const clientId = 'e7c4f61b0cad4ef29625b7f21946d174';
     const redirectUri = "http://localhost:3000";
-    const authEndpoint = "https://accounts.spotify.com/authorize"
     const responseType = "token"
-    const spotify = new SpotifyWebApi();
-    const [spotifyToken, setSpotifyToken] = useState("");
+    const authEndpoint = "https://accounts.spotify.com/authorize"
     const navigate = useNavigate();
 
-
     // ** ADD THE ACTUAL SCOPES **
-    const scopes = ['ugc-image-upload', 'user-read-playback-state', 'user-modify-playback-state', 'user-read-currently-playing', 'app-remote-control', 'streaming', 
-    'playlist-read-private', 'playlist-read-collaborative', 'playlist-modify-private', 'playlist-modify-public', 'user-follow-modify', 'user-follow-read', 
-    'user-read-playback-position', 'user-top-read', 'user-read-recently-played', 'user-library-modify', 'user-library-read', 'user-read-email', 'user-read-private',
-    'user-soa-link', 'user-soa-unlink', 'user-manage-entitlements', 'user-manage-partner', 'user-create-partner'];
+    // const scopes = ['ugc-image-upload', 'user-read-playback-state', 'user-modify-playback-state', 'user-read-currently-playing', 'app-remote-control', 'streaming', 
+    // 'playlist-read-private', 'playlist-read-collaborative', 'playlist-modify-private', 'playlist-modify-public', 'user-follow-modify', 'user-follow-read', 
+    // 'user-read-playback-position', 'user-top-read', 'user-read-recently-played', 'user-library-modify', 'user-library-read', 'user-read-email', 'user-read-private',
+    // 'user-soa-link', 'user-soa-unlink', 'user-manage-entitlements', 'user-manage-partner', 'user-create-partner'];
+    const scopes = 'user-read-recently-played';
+    
 
-    const authURL = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}`
-    // const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token`;
-
+    const authURL = `${authEndpoint}?scope=${scopes}&client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}`
     const getTokenFromUrl = () => { 
         return window.location.hash
             .substring(1)
@@ -34,21 +31,20 @@ const Login = () => {
 
                 return initial;
             }, {});
+
+
     }
-    const apiUrl = 'https://accounts.spotify.com/api/token';
 
     useEffect(() => {
-        console.log("Derived: ", getTokenFromUrl())
+        //console.log("Derived: ", getTokenFromUrl())
         const token = getTokenFromUrl().access_token
         window.location.has = "";
-
         if (token) {
-          console.log("TOKEN: ", token);
-          navigate("/activity", { state: { AccessToken: token } });
-        }
-        
-    })  
-
+            window.localStorage.setItem("token", token);
+            navigate("/activity", { state: { AccessToken: token } });
+        }  
+    })
+    
     return (
         <div className='container'>
             <h1 id='login-title'>Welcome to Listenify</h1>
--- listenify/src/components/Stats/index.js
+++ listenify/src/components/Stats/index.js
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.scss'
-import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 
 const Stats = () => {
-  const location = useLocation();
-  const token = location.state.AccessToken;
-  useEffect(() => {
+  const token = window.localStorage.getItem("token")
+  const [searchKey, setSearchKey] = useState("")
+
+
+  /*useEffect(() => {
     
     if (token) {
       console.log("TOKEN FOUND:", token);
-      const apiEndpoint = 'https://api.spotify.com/v1/me/player/recently-played';
+      const apiEndpoint = 'https://api.spotify.com/v1/search';
+
 
       // Make the Axios GET request
       axios
@@ -31,10 +33,55 @@ const Stats = () => {
     } else {
       console.log("TOKEN IS EMPTY");
     }
+
+    
   })
-    return (
-        <h1>{token}</h1>
-    )
+  */
+
+  const getRecentlyPlayed = async(e) => {
+      // Make the Axios GET request
+      e.preventDefault()
+      axios.get("https://api.spotify.com/v1/me/player/recently-played", {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        .then((response) => {
+          // Handle the response data here
+          const recentlyPlayedTracks = response.data.items;
+          console.log(recentlyPlayedTracks);
+        })
+        .catch((error) => {
+          // Handle any errors
+          console.error('Error fetching recently played tracks:', error);
+        });
+  }
+
+  const searchArtists = async(e) => {
+    e.preventDefault()
+      const {data} = await axios.get("https://api.spotify.com/v1/search", {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        params: {
+          q: searchKey,
+          type: 'artist'
+        }
+        
+      })
+      console.log(data)
+
+  }
+
+  return (
+    <div>
+      {/* <form onSubmit={searchArtists}>
+        <input type='text' onChange={e => setSearchKey(e.target.value)} />
+        <button type={"submit"}>Search</button>
+      </form> */}
+      <button onClick={getRecentlyPlayed}>print recently played!</button>
+    </div>
+  )
 }
 
 export default Stats
